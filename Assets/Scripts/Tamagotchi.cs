b8e5bcd0601927d20e5b30b8553c4ec9fc9ebfa4
P3RYIT3 <248978473+P3RYIT3@users.noreply.github.com>
2026-03-06 12:40:46 +0100

    added hunger/mood/cleanliness countdown + buttons + timeout to feed button

--- Assets/Scripts/Tamagotchi.cs
+++ Assets/Scripts/Tamagotchi.cs
@@ -1,6 +1,8 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 [CreateAssetMenu(fileName = "Tamagotchi", menuName = "Scriptable Objects/Tamagotchi")]
 public class Tamagotchi : MonoBehaviour
@@ -17,20 +19,72 @@ public class Tamagotchi : MonoBehaviour
     [SerializeField] private TMP_Text moodValue;
     [SerializeField] private TMP_Text cleanlinessValue;
 
-    private int hunger;
-    private int mood;
-    private int cleanliness;
+    private float hunger;
+    private float mood;
+    private float cleanliness;
+
+    private float cooldown;
+    private float timer;
 
     public void Awake()
     {
-        hungerSlider.value = hunger = 50;
+        hungerSlider.value = hunger = 100f;
         hungerValue.text = $"{hunger}";
         
-        moodSlider.value = mood = 50;
+        moodSlider.value = mood = 100f;
         moodValue.text = $"{mood}";
         
-        cleanlinessSlider.value = cleanliness = 50;
+        cleanlinessSlider.value = cleanliness = 100f;
         cleanlinessValue.text = $"{cleanliness}";
+
+        cooldown = 2f;
+        timer = 0f;
     }
+
+    public void Update()
+    {
+        hunger -= 1f * Random.Range(1f, 5f) * Time.deltaTime;
+        hungerSlider.value = hunger;
+        hungerValue.text = $"{Math.Round(hunger)}";
+        
+        mood -= Random.Range(1f, 5f) * Time.deltaTime;
+        moodSlider.value = mood;
+        moodValue.text = $"{Math.Round(mood)}";
+        
+        cleanliness -= Random.Range(1f, 5f) * Time.deltaTime;
+        cleanlinessSlider.value = cleanliness;
+        cleanlinessValue.text = $"{Math.Round(cleanliness)}";
+        
+    }
+
+    public void Feed()
+    {
+        if (Time.time >= timer)
+        {
+            switch (hunger + 10)
+            {
+                case >100:
+                    hunger = 100f;
+                    break;
+                default:
+                    hunger += 10;
+                    break;
+            }
+            
+            timer = Time.time + cooldown;
+            Debug.Log(timer);
+        }
+    }
+
+    public void Clean()
+    {
+        cleanliness += 2;
+    }
+
+    public void Play()
+    {
+        mood += 2;
+    }
+    
     
 }
